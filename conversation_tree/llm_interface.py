611b59e2a3544aaaf1097460dacb81d06a776991
Kenneth <kencubed@gmail.com>
2024-12-29 16:55:38 -0500

    removed unused dependency

--- conversation_tree/llm_interface.py
+++ conversation_tree/llm_interface.py
@@ -1,9 +1,6 @@
 import os
-import sys
-print(sys.path[0])
 from dotenv import load_dotenv
 from openai import OpenAI
-from conversation_tree import conversation as conv
 
 load_dotenv() # Load environment variables from a .env file
 
@@ -22,16 +19,5 @@ def query_llm(messages: list[dict[str,str]], model="gpt-4o-mini") -> str:
     return response
 
 if __name__ == "__main__":
-    test_messages = [{'role': 'user',
-                      'content': 'This is my first message. I will refer back to it later.'},
-                      {'role': 'assistant',
-                       'content': "Got it! I'll remember this is your first message for future reference. Let me know how I can assist you!"},
-                       {'role': 'user',
-                        'content': 'This is my second message. I will refer back to it later.'},
-                        {'role': 'assistant',
-                         'content': 'Understood! This is your second message. Feel free to reference it anytime.'},
-                         {'role': 'user',
-                          'content': 'Repeat back to my my messages in order'}
-                      ]
-    test_response = query_llm(test_messages)
-    print(test_response)
+    # {"role": "system", "content": "You are a project assistant."}
+    pass
